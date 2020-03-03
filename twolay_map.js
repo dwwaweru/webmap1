97641e724d47c2acac9902ee05533cce08a6fba2
dwwaweru <dwanja.wawer@gmail.com>
2020-03-02 23:45:06 -0500

    d3: labels for three layers

--- twolay_map.js
+++ twolay_map.js
@@ -1,7 +1,7 @@
 'use strict'        // let the browser know we're serious
 
 // debug statement letting us know the file is loaded
-console.log('Loaded map.js')
+console.log('Loaded twolaymap.js')
 
 // your mapbox token
 mapboxgl.accessToken = 'pk.eyJ1IjoiZHd3YXdlcnUiLCJhIjoiY2s2dnJkdTd0MDBiZzNsbzh0cGIybGZmbSJ9.escl27EkMHCYKwLThisFkw'
